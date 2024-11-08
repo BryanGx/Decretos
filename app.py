e374e369fae4f6265bb280229e5e8a868c0489c4
BryanGx <122252064+BryanGx@users.noreply.github.com>
2024-11-08 00:41:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,4 @@ def generate_decree():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=
-    app.run(
-True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
